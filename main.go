2f443f637bf4ec0b318dc767b466bb80283f9090
Haroon Albar <haroonalbarx@gmail.com>
2024-07-06 20:21:40 +0530

    feat: create a gin router and serve it , handle "/ping" route
    by default engine.Run() looks for port loaded if
    no port is loaded it will server on port 8080

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/haroonalbar/jwtgo/initializers"
 )
 
@@ -13,5 +14,16 @@ func init() {
 }
 
 func main() {
-	fmt.Println("JWT")
+	// create a router
+	r := gin.Default()
+
+	// handle ping
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// start server
+	r.Run()
 }
